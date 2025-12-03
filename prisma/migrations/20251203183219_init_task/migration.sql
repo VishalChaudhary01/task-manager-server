bb7ad5b377d2881b54a1a974ab2c6ffbac784580
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2025-12-04 00:51:27 +0530

    database setup done

---
+++ prisma/migrations/20251203183219_init_task/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PENDING', 'COMPLETED');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Task_title_key" ON "Task"("title");
