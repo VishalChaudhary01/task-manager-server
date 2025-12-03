b684fbb291626323c5b6a25325e3d75de3dfbed3
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2025-12-03 23:34:49 +0530

    centralize error handling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Env } from "./config/Env.config";
+import { AppError, errorHandler } from "./middlewares/error-handler";
+import { StatusCode } from "./config/status-code.config";
 
 const app = express();
 const PORT = Env.PORT;
 
-app.get("/health", (req, res) => {
+app.get("/health", (_req: Request, res: Response) => {
   res.status(200).json({ message: "Healthy server!" });
 });
+
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  next(new AppError(`API route ${req.path} not found`, StatusCode.NOT_FOUND));
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,37 @@
+import { Env } from "@/config/Env.config";
+import { HTTPStatusCode, StatusCode } from "@/config/status-code.config";
+import { NextFunction, Request, Response } from "express";
+
+export class AppError extends Error {
+  public statusCode: HTTPStatusCode;
+
+  constructor(
+    message: string,
+    statusCode: HTTPStatusCode = StatusCode.INTERNAL_SERVER_ERROR
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const errorHandler = async (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(`Error occure at path: ${req.path}`, error);
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  return res.status(StatusCode.INTERNAL_SERVER_ERROR).json({
+    message:
+      Env.NODE_ENV === "development"
+        ? error.message ??
+          "We are sorry for the inconvenience. Something went wrong on the server. Please try again later."
+        : "Unexpected error occure",
+  });
+};
